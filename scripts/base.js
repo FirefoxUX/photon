d443137aa30be6d190a7c430fd4a3b6fdf3830d8
Blake Winton <bwinton@latte.ca>
2015-11-25 15:53:15 -0500

    Add (and style) some colours!

--- scripts/base.js
+++ scripts/base.js
@@ -3,12 +3,16 @@
 'use strict';
 
 $(() => {
-  $('div.components').hide();
-  $('.list-group-item.components').click(() => {
-    $('div.components').slideToggle();
-  });
+  $('.list-group > div').hide();
   $('.list-group').click((e) => {
+    let target = $(e.target);
+    if (target.parent().hasClass('list-group')) {
+      let subclass = Array.prototype.filter.bind(e.target.classList)(e => e !== 'list-group-item');
+      if (subclass.length) {
+        $('div.' + subclass[0]).slideToggle();
+      }
+    }
     $('.list-group .list-group-item').removeClass('list-group-item-info');
-    $(e.target).addClass('list-group-item-info');
+    target.addClass('list-group-item-info');
   });
 });
