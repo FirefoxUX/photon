c05a1c80a0176113f5f185b6198ee23f98bfcab1
Blake Winton <bwinton@latte.ca>
2015-12-13 14:25:20 -0500

    Change the styles, and fix some eslint-react errors.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ const TableOfContents = React.createFactory(require('./TableOfContents.jsx'));
 const Editor = React.createFactory(require('./Editor.jsx'));
 
 const App = React.createClass({
+  displayName: 'App',
   getInitialState: function() {
     const sources = generateSources();
     return { sources: sources,
@@ -25,25 +26,26 @@ const App = React.createClass({
     const { sources, selectedSourceName } = this.state;
     const selectedSource = sources.find(source => source.name === selectedSourceName);
 
-    return <div className="app">
+    return (<div className="app">
       <div className="toolbar">
         <div className="title">
-          <h2>Firefox<br/>Style Guide v1.0</h2>
-          <small>Updated today</small>
+          <h2>{"Firefox"}<br/>{"Style Guide v1.0"}</h2>
+          <small>{"Updated today"}</small>
         </div>
         <div className="section">
-          <input type="search" placeholder="🔍 Search the style guide"></input>
+          <input placeholder="🔍 Search the style guide"
+              type="search"
+          ></input>
         </div>
       </div>
-      <div>
-        <div className="hbox">
-          <TableOfContents items={sources}
+      <div className="content">
+        <TableOfContents items={sources}
+            onItemSelected={this.handleItemSelected}
             selectedItem={selectedSource}
-            selectItem={this.handleItemSelected} />
-          <Editor source={selectedSource} />
-        </div>
+        />
+        <Editor source={selectedSource} />
       </div>
-    </div>;
+    </div>);
   }
 });
 
--- src/components/TableOfContents.jsx
+++ src/components/TableOfContents.jsx
@@ -3,12 +3,12 @@ const dom = React.DOM;
 
 const TableOfContents = React.createClass({
   render: function() {
-    const { items, selectedItem } = this.props;
+    const { items, selectedItem, onItemSelected } = this.props;
 
     return <ul className="toc">
       {items.map((item, i) => {
         return <li key={i} className={(item === selectedItem) ? 'selected' : ''}
-          onClick={() => this.props.selectItem(item)}>{item.name}</li>
+          onClick={() => onItemSelected(item)}>{item.name}</li>
       })}
     </ul>
   }
