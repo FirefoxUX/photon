d510803dc084f89d1fc5f6a119554d7aa0a8e21d
Blake Winton <bwinton@latte.ca>
2015-12-13 14:14:31 -0500

    Load the content from json.

--- src/components/generateSources.js
+++ src/components/generateSources.js
@@ -1,38 +1,9 @@
-const foo = function () {
-  var x = 5;
-  var y = 6;
-  return x + y;
-}.toString();
+'use strict';
 
-const bar = function(x, y) {
-  if(Math.random() < .5) {
-    return Math.floor(x / y);
-  }
-  return x / y;
-}.toString();
-
-const baz = function (actor) {
-  var source = getSource(actor);
-  if(source.url) {
-    addSource(source);
-  }
-}.toString();
-
-const sourceNames = ['Home', 'Visual Design', 'Components', 'Patterns', 'Resources']
-const sourceTexts = [foo, bar, baz]
+var data = require('json!../../contents/index.json');
 
 function generateSources() {
-  const numSources = 5;
-  const sources = [];
-
-  for(let i = 0; i<numSources; i++) {
-    sources.push({
-      name: sourceNames[i],
-      text: sourceTexts[Math.random() * sourceTexts.length | 0]
-    });
-  }
-
-  return sources;
+  return data.map(e => ({name: e.title, text: e.file}));
 }
 
 module.exports = generateSources;
