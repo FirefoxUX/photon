940f5e660e244bc6b1a6da14ac5ab51043e9fd07
Blake Winton <bwinton@latte.ca>
2015-12-18 15:29:01 -0500

    Muck with the z-index to let us select colours under the popup.

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -27,7 +27,9 @@ const Editor = React.createClass({
     let node = ReactDOM.findDOMNode(this);
     Array.from(node.querySelectorAll('.colours, .multi-swatch')).map(e => {
       e.addEventListener('click', (evt) => {
-        if (!evt.target.classList.contains('colour')) {
+        var popup = node.querySelector('.popup');
+        if (!evt.target.classList.contains('colour') ||
+          popup.style.zIndex) {
           return;
         }
         let copyElement = document.createElement('input');
@@ -37,13 +39,16 @@ const Editor = React.createClass({
         copyElement.select();
         document.execCommand('copy');
         copyElement.remove();
-        var popup = node.querySelector('.popup');
         popup.textContent = `Copied ${evt.target.textContent} to clipboard.`;
+        popup.style.zIndex = '2';
         popup.style.opacity = 1;
         var scrollPosition = document.body.scrollTop || document.documentElement.scrollTop;
         popup.style.top = `calc(${scrollPosition}px + 50vh - .5em)`;
         setTimeout(() => {
           popup.style.opacity = '';
+          setTimeout(() => {
+            popup.style.zIndex = '';
+          }, 500);
         }, 1500);
       });
     });
