4c9848f0b3296d40f378998b184d395e2942d194
Blake Winton <bwinton@latte.ca>
2016-02-10 11:20:48 -0500

    Add the first pass at some glyphs!

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -5,6 +5,7 @@
 
 require('../styles/editor.scss');
 require('../styles/controls.scss');
+require('../styles/glyphs.scss');
 require('../styles/inputs.scss');
 require('../styles/panels.scss');
 require('../../node_modules/highlight.js/styles/color-brewer.css');
