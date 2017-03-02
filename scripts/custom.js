a929617c1deb39ec9bb2fbae7b3e00833a8c1623
Amin Al Hazwani <amin.alhazwani@gmail.com>
2017-03-02 18:28:12 +0100

    [fix] body overflow when nav is open

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,13 +1,13 @@
 function showNav() {
   document.getElementById("nav").style.left = "0";
   document.getElementById("overlay").classList.remove("dn");
-  document.getElementById("body").classList.add("overflow-hidden");
+  document.getElementsByTagName("body")[0].classList.add("overflow-hidden");
 }
 
 function hideNav() {
   document.getElementById("nav").style.left = "-100%";
   document.getElementById("overlay").classList.add("dn");
-  document.getElementById("body").classList.remove("overflow-hidden");
+  document.getElementsByTagName("body")[0].classList.remove("overflow-hidden");
 }
 
 function checkWidth() {
