267c9c289bd7b39f882afbb6b6597fc286c59f37
Amin Al Hazwani <amin.alhazwani@gmail.com>
2017-03-06 12:58:17 +0100

    [style] palette explorations

--- src/components/TableOfContents.jsx
+++ src/components/TableOfContents.jsx
@@ -40,7 +40,7 @@ const ListItem = connect(state => {
     return (<div className={((page && item.title === page.category) ? ' selected' : '') +
               (this.props.expanded ? ' expanded' : '')}
             >
-      <p className="fw5 white font-smoothing ma0 pv2"
+      <p className="fw5 ma0 pv2"
           onClick={handleClick}
       >{item.title}</p>
       {item.pages.map(this.getPage)}
@@ -74,7 +74,7 @@ const Page = connect(state => {
 
     const url = `/${item.file}`;
     return (<Link activeClassName="active"
-        className={'db no-underline moon-gray font-smoothing fw4 pv2' + ((item === page) ? ' selected' : '')}
+        className={'db no-underline gray fw4 pv2' + ((item === page) ? ' selected' : '')}
         key={pages.indexOf(page) + ':' + i}
         to={url}
             ><span className="pl3">{item.title}</span></Link>);
@@ -121,26 +121,26 @@ const TableOfContents = React.createClass({
     let items = sources.map(getItem);
 
     return (<nav 
-        className="bg-dark-gray h-100 w-100 z-max order-1 order-0-l w-6-l"
+        className="bg-near-white near-black h-100 w-100 z-max order-1 order-0-l w-6-l"
         id="nav"
             >
       <div className="flex flex-column h-100 center mw7 pa3 pa4-l">
         <div className="self-start dn db-l">
           <p className="f4 fw5 lh-solid ma0">
-            <a className="white font-smoothing no-underline" 
+            <a className="near-black no-underline" 
                 href="/#/welcome"
             >{'Firefox Design System'}
             </a>
           </p>
-          <p className="white font-smoothing f6 lh-copy ttu fw5 mt2 mb4">{'starting v57 (photon)'}</p>
+          <p className="f6 lh-copy ttu fw5 mt2 mb4">{'starting v57 (photon)'}</p>
         </div>
         <div className="self-stretch overflow-y-scroll h-100 mb5">
           {items}
         </div>
         <div className="self-end w-100">
-          <p className="white font-smoothing lh-copy ma0 fw4">
+          <p className="lh-copy ma0 fw4">
             {'Questions, doubts or feedback? '}
-            <a className="white font-smoothing no-underline fw5" 
+            <a className="near-black no-underline fw5" 
                 href="https://github.com/bwinton/StyleGuide/issues"
             >{'Open an issue on GitHub!'}
             </a>
