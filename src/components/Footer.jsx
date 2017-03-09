0c9dc9ca4e0a9e311472388ccf9ae84748e7c51b
Amin Al Hazwani <amin.alhazwani@gmail.com>
2017-03-09 16:44:49 +0100

    [style] minor edit footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,19 +21,19 @@ const Footer = connect(state => {
     return(
       <footer className="fira">
         <div className="center mw7 ph3 pb3-ns cf">
-          <p className="fl w-50 tl mt0">
-            <span className="pr2 code">&#8592;</span>
+          <p className="fl w-50 fw5 tl mt0">
+            <span className="pr2 pr3-ns code fw6">&#8592;</span>
             <Link activeClassName="fw5"
                 className="no-underline black"
                 to={getUrl(this.props.previous_page)}
             >{this.props.previous_page.title}
             </Link>
           </p>
-          <p className="fl w-50 tr mt0">
+          <p className="fl w-50 fw5 tr mt0">
             <Link activeClassName="fw5"
                 className="no-underline black"
                 to={getUrl(this.props.next_page)}
-            >{this.props.next_page.title}<span className="pl2 code">&#8594;</span>
+            >{this.props.next_page.title}<span className="pl2 pl3-ns code fw6">&#8594;</span>
             </Link>
           </p>
         </div>
