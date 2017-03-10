7e084c50a6f40befc93a1a0b0f276f8676e0c9c1
Amin Al Hazwani <amin.alhazwani@gmail.com>
2017-03-10 11:28:11 +0100

    [fix] back to home link

--- src/components/TableOfContents.jsx
+++ src/components/TableOfContents.jsx
@@ -146,7 +146,7 @@ const TableOfContents = React.createClass({
         <div className="self-start dn db-l">
           <p className="f4 fw5 lh-solid ma0">
             <a className="near-black no-underline" 
-                href="/welcome.html"
+                href="/"
             >{'Firefox Design System'}
             </a>
           </p>
