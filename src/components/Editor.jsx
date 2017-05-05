e07d1b5d94fbbc790d7f25d2d22aaf175af5cba8
Blake Winton <bwinton@latte.ca>
2017-05-05 12:30:25 -0400

    Add click-to-copy functionality.

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -33,6 +33,7 @@ const Editor = React.createClass({
         }
       }
     }, false);
+    this.addClickToCopy();
   },
 
   componentDidUpdate: function(prevProps) {
@@ -62,8 +63,8 @@ const Editor = React.createClass({
 
     if (node.querySelector('header') && !node.querySelector('header[toc-none]')) {
       let header_list = document.createElement('ul');
-      header_list.addEventListener('click', (e) => {
-        if (e.target.tagName === "A") {
+      header_list.addEventListener('click', e => {
+        if (e.target.tagName === 'A') {
           sendClick('header-click', e.target.getAttribute('href').substring(1))
         }
       });
@@ -71,6 +72,24 @@ const Editor = React.createClass({
       header_list.innerHTML = header_links.join('\n');
       node.querySelector('header').appendChild(header_list);
     }
+  },
+
+  addClickToCopy: function () {
+    let node = ReactDOM.findDOMNode(this);
+    node.addEventListener('click', e => {
+      if (e.target.tagName === 'CODE') {
+        let text = e.target.textContent;
+        let copyElement = document.createElement('input');
+        copyElement.setAttribute('type', 'text');
+        copyElement.setAttribute('value', text);
+        copyElement = document.body.appendChild(copyElement);
+        copyElement.select();
+        document.execCommand('copy');
+        copyElement.remove();
+        e.target.classList.add('copied');
+        setTimeout(() => {e.target.classList.remove('copied')}, 2000);
+      }
+    });
   },
 
   render: function() {
